22171db1c25e501aed1e86a88eb155a52ad6c474
bob <bob@bobvann.noip.me>
2017-03-05 14:05:23 +0100

    bidirectional communication between sockets

--- daemon/GPIOClass.py
+++ daemon/GPIOClass.py
@@ -1,25 +1,39 @@
 import socket, struct
+import time
 
 class GPIOClass:
 	#DEFINITIONS ON HOW TO SET THE GPIO OUT IN ORDER TO IRRIGATE
 	#STATE_ON MEANS IRRIGATING, AND IS HOW GPIO NEEDS TO BE SET UP TO IRRIGATE
-	#MOST TIME 1=HIGH AND RELAY=CLOSE -> IRRIGATING
-	#SOMETIMES IN ORDER TO CLOSE THE RELAY YOU NEED TO SET IT UP AS 0
+	#MOST TIME ON=HIGH AND RELAY=CLOSE -> IRRIGATING
+	#SOMETIMES IN ORDER TO CLOSE THE RELAY YOU NEED TO SET IT UP AS LOW
 
 	STATE_ON = "LOW"
 	STATE_OFF = "HIGH"
+	STATE_ERR = "ERR"
+
+
+	STR_GETVAL = "GET"
 
 	socketpath="/run/rpirrigate-gpiod-socket"
 
 	def __init__(self):
-		self.client = socket.socket( socket.AF_UNIX, socket.SOCK_DGRAM )
+		self.client = socket.socket( socket.AF_UNIX, socket.SOCK_STREAM )
 		self.client.connect( self.socketpath )
 
 	def openWater(self, pin):
 		pin = str(pin)
-		self.client.send(self.STATE_ON+"#"+pin)
+		self.client.send(self.STATE_ON+"#"+pin+"#")
 
 	def closeWater(self, pin):
 		pin = str(pin)
-		self.client.send(self.STATE_OFF+"#"+pin)
+		self.client.send(self.STATE_OFF+"#"+pin+"#")
 
+	def getState(self,pin):
+		pin = str(pin)
+		self.client.send(self.STR_GETVAL+"#"+pin+"#")
+		datagram = self.client.recv( 1024 )
+		d=datagram.split("#")
+		if len(d)<1:
+			return self.STATE_ERR
+		else:
+			return d[0]
--- daemon/RPirrigate-gpiod.py
+++ daemon/RPirrigate-gpiod.py
@@ -3,7 +3,12 @@ import os, os.path
 import time,stat
 import RPi.GPIO as GPIO
 
+#importing for constraints
+from GPIOClass import GPIOClass
+
 socketpath = "/run/rpirrigate-gpiod-socket"
+
+os.chmod(socketpath,stat.S_IRWXO)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -16,21 +21,50 @@ def set_low(gp):
 	GPIO.setup(gp,GPIO.OUT)
 	GPIO.output(gp,0)
 
+def get_value(gp):
+	val = 1
+	if val==0:
+		return GPIOClass.STATE_ON+"#"
+	elif val==1:
+		return GPIOClass.STATE_OFF+"#"
+	else:
+		return GPIOClass.STATE_ERR+"#"
+
 
 if os.path.exists(socketpath):
 	os.remove( socketpath )
 
-server = socket.socket( socket.AF_UNIX, socket.SOCK_DGRAM )
+server = socket.socket( socket.AF_UNIX, socket.SOCK_STREAM )
 server.bind(socketpath)
-os.chmod(socketpath,stat.S_IRWXO)
+
+
+server.listen(5)
 
 while True:
-	datagram = server.recv( 1024 )
-	d=datagram.split("#")
+	conn, addr  = server.accept()
 
-	if str(d[0])=="LOW":
-		set_low(int(d[1]))
-	elif str(d[0])=="HIGH":
-		set_high(int(d[1]))  
+	while True:
+
+		datagram = conn.recv( 1024 )
+
+
+		if not datagram:
+			break
+		
+		d=datagram.split("#")
+
+		print d
+
+		if(len(d)<2):
+			break
+
+		if str(d[0]) == GPIOClass.STATE_ON :
+			set_low(int(d[1]))
+		elif str(d[0]) == GPIOClass.STATE_OFF:
+			set_high(int(d[1]))
+		elif str(d[0]) == GPIOClass.STR_GETVAL:
+			val = get_value(int(d[1]))
+
+			conn.send( str(val) )
 
 server.close()
